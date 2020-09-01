9e6346c8aca96032c400f0b35cb1e13981f803df
RogerioBorba <rogerio.borba17@gmail.com>
2020-08-31 21:36:50 -0300

    clean prj

--- generator/util.py
+++
@@ -1,8 +0,0 @@
-import re
-def convert_camel_case_to_hifen(camel_case_string):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', camel_case_string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
-
-def convert_camel_case_to_underline(camel_case_string):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', camel_case_string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
--- src/middlewares/security.py
+++
@@ -1,6 +0,0 @@
-from secure import SecureHeaders
-secure_headers = SecureHeaders()
-def setup_middlewares(app):
-    @app.middleware('response')
-    async def set_secure_headers(request, response):
-        secure_headers.sanic(response)
