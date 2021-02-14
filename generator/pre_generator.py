554f3b4e59d324b7ea379b89ee21e307e4e70984
GabrielSalvadorCardoso <gabrielcardoso95@gmail.com>
2021-02-14 11:08:51 -0300

    Handler to relationship fields in code generator

--- generator/pre_generator.py
+++ generator/pre_generator.py
@@ -5,6 +5,12 @@ from sqlalchemy.orm.attributes import InstrumentedAttribute
 def is_geo_class(a_class:DeclarativeMeta):
     model_attrs = [getattr(a_class, element) for element in dir(a_class) if isinstance(getattr(a_class, element), InstrumentedAttribute)]
     for attr in model_attrs:
-        if(hasattr(attr.property.columns[0].type, "geometry_type")):
-            return True
+        try:
+            if(hasattr(attr.property.columns[0].type, "geometry_type")):
+                return True
+        except AttributeError:
+            pass
+            # for ele in dir(attr.property):
+            #     print(ele)
+        # print()
     return False
